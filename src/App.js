f454bd6bf5340ef4b6ada5857bbc7b1fd809dc59
cometoflegend <irenesg83@educastur.es>
2024-10-07 21:40:14 +0200

    modificados archivos app y square en src. arreglar errores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, {replaceAt, parameters} from 'react';
 import './App.css';
+import Square from './Square';
+import useState from 'react';
+const guesses = ["     ", "     ","     " , "     ", "     "];
+const solution='CLASE';
+
 
 function App() {
+
+  const [currentAttempt, setCurrentAttempt] = useState(1);
+  const [messageWinner, setMessageWinner] = useState("");
+
+  const handleSquareChange=(parameters) =>
+    
+    guesses[parameters.row-1]= replaceAt(guesses[parameters.row-1], parameters.column-1,parameters.letter);
+      let isAttempComplete=true;
+      for(let i=0;i<5;i++)
+      {
+        if(guesses[parameters.row-1].charAt(i)==" ")
+          {
+            isAttempComplete=false;
+            break;
+          }
+      }
+      if (isAttempComplete) 
+        {
+          if(guesses[parameters.row-1] == solution)
+          {
+            setMessageWinner("CONGRATULATIONS");
+            setCurrentAttempt(10); 
+          }          else 
+          {
+            if (currentAttempt<5) 
+              {
+                setCurrentAttempt(currentAttempt+1);
+              } 
+              else
+              {
+                setMessageWinner("Game Over");
+                setCurrentAttempt(10); 
+              }
+          }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WORDLE
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>
+        <div className='board'>
+          <div id="row"  className='row' > 
+            <Square row="1" column="1" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="1" column="2" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="1" column="3" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="1" column="4" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="1" column="5" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+          </div>
+          <div id="row" className='row'  > 
+            <Square row="2" column="1" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="2" column="2" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="2" column="3" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="2" column="4" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="2" column="5" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+          </div>
+          <div id="row" className='row'  > 
+            <Square row="3" column="1" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="3" column="2" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="3" column="3" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="3" column="4" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="3" column="5" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+          </div>
+          <div id="row" className='row'  > 
+            <Square row="4" column="1" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="4" column="2" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="4" column="3" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="4" column="4" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="4" column="5" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+          </div>
+          <div id="row" className='row'  > 
+            <Square row="5" column="1" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="5" column="2" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="5" column="3" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="5" column="4" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+            <Square row="5" column="5" attempt={currentAttempt} textUpdate={(e) => handleSquareChange(e)}></Square>
+          </div>
+          </div>
+      </p>
+
     </div>
-  );
-}
+  )
+} 
 
 export default App;
